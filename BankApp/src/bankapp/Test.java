6e6b14afb45baedb6593fde349b57f4749fb0f42
KamilJankowski99 <kjankowski@wow.wroclaw.pl>
2020-03-06 18:16:36 +0100

    rozpakowana poprawka Dawida

--- BankApp/src/bankapp/Test.java
+++ BankApp/src/bankapp/Test.java
@@ -1,5 +1,7 @@
 package bankapp;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -8,11 +10,96 @@ import java.util.logging.Logger;
 
 public class Test {
 
-    public Scanner s;
+    public Scanner s; 
     public Random rand;
+    public static Account login(){//metoda pozwala wybrac konto na podstawie jego numeru(nazwie pliku) i utworzyc nowy obiekt wypelniony odpowiednio danymi
+        boolean ch=false;
+        Account ac1= new Account();
+        while (!ch){
+        Scanner nr = new Scanner(System.in);
+            try{
+                System.out.print("Podaj numer konta: ");
+                String numer = nr.nextLine();
+                numer= numer+".txt";
+                Scanner f = new Scanner(new File(numer));
+                ac1.setName(f.nextLine());
+                ac1.setSurname(f.nextLine());
+                ac1.setAccnumber(f.nextLine());
+                ac1.setCnumber(f.nextLine());
+                ac1.setBalance(f.nextInt());
+                ch=true;
+        }
+        catch(FileNotFoundException e){
+            System.out.println("");
+            System.out.println("Konto nie istnieje"); 
+            System.out.println("");
+            nr.reset();
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        }   
+        }
+        return ac1;
+    }
+    
+    
+    public static void update(Account a,int newbal){//metoda zmienia saldo w odpowiednim pliku
+        String num = a.getAccnumber();
+        num=num+".txt"; 
+        try{
+            
+            PrintWriter pw= new PrintWriter(num);
+            pw.println(a.getName());
+            pw.println(a.getSurname());
+            pw.println(a.getAccnumber());
+            pw.println(a.getCnumber());
+            pw.println(newbal);
+            pw.close();
+            
+        }
+        catch(FileNotFoundException e){
+            System.out.println("nie wiem jak, ale pliku brak :/");
+        }
+        
+    }
+    public static void newClient(){//metoda tworzy nowy plik klienta nazwany losowym numerem konta
+        Random rac= new Random();
+        String acnum="";
+        String cnum="";
+        for(int y=0;y<13;y++){
+            int pn= rac.nextInt(89)+10;
+            acnum=acnum+Integer.toString(pn);
+        }
+        for(int y=0;y<8;y++){
+            int pn= rac.nextInt(89)+10;
+            cnum=cnum+Integer.toString(pn);
+        }
+        Scanner cl= new Scanner(System.in);
+        System.out.println("");
+        System.out.print("Podaj imię: ");
+        String cname=cl.nextLine();
+        System.out.print("Podaj nazwisko: ");
+        String csname=cl.nextLine();
+        System.out.print("Podaj saldo: ");
+        String cbalance=cl.nextLine();
+        try{
+            
+            PrintWriter pw= new PrintWriter(acnum+".txt");
+            pw.println(cname);
+            pw.println(csname);
+            pw.println(acnum);
+            pw.println(cnum);
+            pw.println(cbalance);
+            pw.close();
+            System.out.println("");
+            System.out.println("Konto zostało utworzone pomyślnie");
+            System.out.println("Twoj numer konta to: "+acnum);
+            System.out.println("Twoj numer karty to: "+cnum);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("nie wiem jak, ale pliku brak :/");
+        }
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner s = new Scanner(System.in); 
         Random rand = new Random();
         int initialBalanceA = 50000;
         int initialBalanceB = 50000;
@@ -21,7 +108,7 @@ public class Test {
         int withdrawAmountB = 0;
         int depositAmountA = 0;
         int depositAmountB = 0;
-
+            
         Account acA = new Account("John", "Doe", initialBalanceA, "12345678901234567890123456");
         Account acB = new Account("John", "Doe", initialBalanceB, "12345678901234567890123456");
         Bank bank = new Bank(1);
@@ -70,5 +157,14 @@ public class Test {
         int expectedBalanceB = initialBalanceB + transferAmount + depositAmountB - withdrawAmountB;
         System.out.println("Spodziewane saldo konta A: " + expectedBalanceA);
         System.out.println("Spodziewane saldo konta B: " + expectedBalanceB);
+        /////////////////////////////////////////////////////////
+        //Account ac1=login();//wprowadzenie konta A, ktorego maja dotyczyc wszystkie podstawowe operacje(dopiero po wyborze opcji przelewu wykonywana bedzie podobna metoda pobierajaca dane z B)
+        //System.out.println("Name: "+ac1.getName()+" Surname: "+ac1.getSurname()+" Account Number: "+ac1.getAccnumber()+" Card Number: "+ac1.getCnumber()+" Balance: "+ac1.getBalance());
+        //int nb=15000;//przykladowe nowe saldo konta
+        //update(ac1,nb);//przyklad wprowadzania nowego salda konta po wykonaniu wybranej operacji
+        //newClient();//przyklad tworzenia konta
     }
-}
+        
+        
+    } 
+
